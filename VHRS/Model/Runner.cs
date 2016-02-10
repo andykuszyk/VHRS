400212ada18acb760b4e47e93602dd897996eed4
andy.kuszyk <andy.kuszyk@galleria-rts.com>
2016-02-10 20:34:39 +0000

    Added validation method for Name

--- VHRS/Model/Runner.cs
+++ VHRS/Model/Runner.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using VHRS.Resources;
 
 namespace VHRS.Model
 {
@@ -23,6 +25,11 @@ namespace VHRS.Model
         /// The fractional odds price represented as a string fraction, e.g. 1/2.
         /// </summary>
         private String _odds;
+
+        /// <summary>
+        /// The maximum number of characters that <see cref="Name"/> can have.
+        /// </summary>
+        private const Int32 _nameLength = 18;
 
         #endregion
 
@@ -82,6 +89,42 @@ namespace VHRS.Model
 
         #endregion
 
+        #region Events
+        
+        /// <summary>
+        /// Fired if a property changes
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Returns an error message describing the validation error with the <see cref="Name"/>
+        /// property, or null if <see cref="Name"/> is valid.
+        /// </summary>
+        /// <returns></returns>
+        private String ValidateName()
+        {
+            if(Name == null)
+            {
+                return String.Empty;
+            }
+            else if (Name.Length > _nameLength)
+            {
+                return String.Format(Language.StringTooLong, _nameLength);
+            }
+            else if (new Regex(@"\d").IsMatch(Name))
+            {
+                return Language.StringContainsNumbers
+            }
+            else
+            {
+                return String.Empty;
+            }
+        }
+
+        #endregion
     }
 }
