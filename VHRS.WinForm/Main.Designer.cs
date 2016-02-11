155099b03ec8e9663b56c8c4e57d0bbae2b38ee6
andy.kuszyk <andy.kuszyk@galleria-rts.com>
2016-02-11 12:43:49 +0000

    Renamed Form1 to Main

--- VHRS.WinForm/Main.Designer.cs
+++ VHRS.WinForm/Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace VHRS.WinForm
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
--- VHRS.WinForm/Main.cs
+++ VHRS.WinForm/Main.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace VHRS.WinForm
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
