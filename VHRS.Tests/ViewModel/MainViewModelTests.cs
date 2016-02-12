0a4b4b9e99961fad52546f23159d473736ea3e17
andy.kuszyk <andy.kuszyk@galleria-rts.com>
2016-02-12 08:25:53 +0000

    Added test for RunRace

--- VHRS.Tests/ViewModel/MainViewModelTests.cs
+++ VHRS.Tests/ViewModel/MainViewModelTests.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VHRS.ViewModel;
 
 namespace VHRS.Tests.ViewModel
 {
-    class MainViewModelTests
+    [TestFixture]
+    public class MainViewModelTests
     {
+        [Test]
+        public void RunRace_RunnerShouldWinWithin2PointsOfProbability()
+        {
+            var viewModel = new MainViewModel();
+            for (Int32 i = 0; i < 4; i++) viewModel.AddRunner();
+            viewModel.Runners[0].Name = "Runner One";
+            viewModel.Runners[0].Odds = "1/2";
+            viewModel.Runners[1].Name = "Runner Two";
+            viewModel.Runners[1].Odds = "2/1";
+            viewModel.Runners[2].Name = "Runner Three";
+            viewModel.Runners[2].Odds = "3/1";
+            viewModel.Runners[3].Name = "Runner Four";
+            viewModel.Runners[3].Odds = "8/1";
+
+            Int64 runnerOneWinCount = 0;
+            for(Int32 i = 0; i < 1000000; i++)
+            {
+                var winner = viewModel.RunRace();
+                if (winner == viewModel.Runners[0]) runnerOneWinCount++;
+            }
+
+            runnerOneWinCount.Should().BeGreaterOrEqualTo(460000);
+            runnerOneWinCount.Should().BeLessOrEqualTo(500000);
+        }
     }
 }
